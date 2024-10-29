5f5b8caa64d017b2ca06328ea9ca8a280017a2c2
Andrey <codenomnom@users.noreply.github.com>
2024-10-29 22:07:07 +0200

    fix: font subset

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Poppins } from "next/font/google";
 const poppins = Poppins({
   weight: ["400", "500", "600", "800"],
   display: "swap",
+  subsets: ["latin"],
   variable: "--font-poppins",
 });
 
