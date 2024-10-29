2d38296c7b592967a50529e51223efff42e1e251
Andrey <codenomnom@users.noreply.github.com>
2024-10-29 22:07:15 +0200

    chore: proper item link

--- src/components/ui-libs-list/UILibsList.tsx
+++ src/components/ui-libs-list/UILibsList.tsx
@@ -11,7 +11,7 @@ export function UILibsList({ items = [] }: Props<ListProps>) {
     <ul className="w-full">
       {items.map((lib) => (
         <li key={lib._meta.path} className="min-h-24 text-[#545454] bg-white rounded mb-4 shadow-[0px_0px_10px_0px_#523f690d] border border-[#e2e2e2] hover:border-[#cfcfcf] hover:shadow-[0px_0px_16px_0px_#523f6926] transition-shadow duration-300">
-          <a href={`/posts/${lib._meta.path}`} className="w-full h-full flex items-start p-6">
+          <a href={`${lib.website}`} target="_blank" className="w-full h-full flex items-start p-6">
             <div className={`logo h-14 w-14 rounded-lg flex justify-center items-center bg-[#fafafa] border border-[#e2e2e2]`}>
               {
                 lib.logo && (
