41690b072779612c2b7e44648305439493f274c5
Andrey <codenomnom@users.noreply.github.com>
2024-10-29 19:54:19 +0200

    feat: first ui libs defined

--- src/content/ui-libs/index.ts
+++ src/content/ui-libs/index.ts
@@ -1,8 +1,8 @@
-import { allUis } from 'content-collections';
+import { allUis, Ui } from 'content-collections';
 
 // https://github.com/sdorra/content-collections/issues/373
 // newest / updated item is last, so it overrides the initial one
-export const ui = Object.values( // convert to array
+export const ui: Array<Ui> = Object.values( // convert to array
   Object.fromEntries( // convert to object
     allUis.map((item) => [ // override array objects to tuples
       item._meta.filePath.replaceAll('\\', '/'), // key is posix file path
@@ -10,3 +10,5 @@ export const ui = Object.values( // convert to array
     ])
   )
 );
+
+export const ui2 = allUis;
