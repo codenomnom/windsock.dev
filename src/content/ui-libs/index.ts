3717cf5c8078c56d2ca71d4dd70e5c1e411ce226
Andrey <codenomnom@users.noreply.github.com>
2024-11-06 20:07:31 +0200

    chore: remove content item filtering
    There was a bug in the library, and saving an item on Windows was causing duplicate content entries. It's now fixed, and thus removing the filtering

--- src/content/ui-libs/index.ts
+++ src/content/ui-libs/index.ts
@@ -1,14 +1,3 @@
 import { allUis, Ui } from 'content-collections';
 
-// https://github.com/sdorra/content-collections/issues/373
-// newest / updated item is last, so it overrides the initial one
-export const ui: Array<Ui> = Object.values( // convert to array
-  Object.fromEntries( // convert to object
-    allUis.map((item) => [ // override array objects to tuples
-      item._meta.filePath.replaceAll('\\', '/'), // key is posix file path
-      item, // value is the item
-    ])
-  )
-);
-
-export const ui2 = allUis;
+export const ui = allUis;
