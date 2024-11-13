a7a80fd72f257f51328726fee4bcffa17e9bd49d
Andrey <codenomnom@users.noreply.github.com>
2024-11-13 23:03:43 +0200

    fix: build failed because of linting

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -2,6 +2,7 @@ import type { SVGProps } from 'react';
 import { cn } from '@/utils';
 
 export const Icon = ({ name, className, ...props }: { name: string, className?: string }) => <span className={cn(name, className)} {...props}> </span>
+// eslint-disable-next-line react/display-name
 const icon = (name: string) => (props: SVGProps<SVGSVGElement>) => <Icon name={name} {...props} />;
 
 export const IconifyIconBaselinePaid = icon('i-ic-baseline-paid');
