b5b2df6f3e0f4856554b7e9dbc064edd2fb60632
Andrey <codenomnom@users.noreply.github.com>
2024-11-14 15:57:41 +0200

    feat: added skill icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
-import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/react";
-import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
+import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [
     iconsPlugin({
-      collections: getIconCollections(["meteocons", "mi", "ic"])
+      collections: getIconCollections(['meteocons', 'mi', 'ic', 'skill-icons'])
     }),
     nextui(),
   ],
